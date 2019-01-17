4bf333c48404decae7e67ba6513176957b8ad07f
Patrick Kim <pkim050@gmail.com>
2019-01-17 20:06:02 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -11,7 +11,7 @@ def loop_iterator(number_of_times)
   counter = 0
   phrase = "Welcome to Flatiron School's Web Development Course!"
   loop do
-    pus phrase
+    puts phrase
     counter+=1
     break if counter == number_of_times
   end
